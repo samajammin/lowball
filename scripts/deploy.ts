7b43fd9d851b2b5c1e17e7708f418aa042a92bb2
Sam Richards <sbrichards@gmail.com>
2020-04-24 09:20:27 -0700

    Update deploy script to store abi in frontend

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,31 +1,42 @@
-import { ethers } from "@nomiclabs/buidler";
-import fs from "fs";
+import { ethers } from "@nomiclabs/buidler"
+import { readArtifact } from "@nomiclabs/buidler/plugins"
+import fs from "fs"
 
 async function main() {
-  const factory = await ethers.getContract("Greeter");
+  const factory = await ethers.getContract("Greeter")
 
   // If we had constructor arguments, they would be passed into deploy()
-  let contract = await factory.deploy("My custom greeting");
+  let contract = await factory.deploy("My custom greeting")
 
   // The address the Contract WILL have once mined
-  console.log("Contract address: ", contract.address);
+  console.log("Contract address: ", contract.address)
 
   // The transaction that was sent to the network to deploy the Contract
-  console.log(contract.deployTransaction.hash);
+  console.log(contract.deployTransaction.hash)
 
   // The contract is NOT deployed yet; we must wait until it is mined
-  await contract.deployed();
+  await contract.deployed()
 
-  // Save the address to access it from the frontend
+  // TODO use relative path
+  const artifact = await readArtifact(
+    "/Users/samrichards/code/lowball/artifacts/", // TODO update
+    "Greeter"
+  )
+
+  // Save the address & abi to access it from the frontend
   fs.writeFileSync(
-    __dirname + "/../app/packages/react-app/src/contract-address.json",
-    JSON.stringify({ Greeter: contract.address }, undefined, 2)
-  );
+    __dirname + "/../app/packages/react-app/src/artifacts.json",
+    JSON.stringify(
+      { Greeter: { address: contract.address, abi: artifact.abi } },
+      undefined,
+      2
+    )
+  )
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error: Error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
